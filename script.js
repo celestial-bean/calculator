daa5df36ad1a4617a20f58c2040d53057dd0dc60
logan <logan326garcia@gmail.com>
2023-08-07 14:44:36 -0500

    .

--- script.js
+++ script.js
@@ -25,32 +25,14 @@ return num1 * num2;
 
 
 
-function operate(data){
-    var haveNum1=false;
+function solve(data){
+    
     data=data.replace(/\s/g,'')
     
-    var validData=[1,2,3,4,5,6,7,8,9,0]
-    var num1=0;
-    var leftOff=0;
-    var num2=0;
+    var validNums=[1,2,3,4,5,6,7,8,9,0];
 
-
-    for (var i=0;i<data.length; i++){
-        
-        if (!haveNum1 && Number(data[i]) in validData){
-        num1=data.slice(0,i+1)
-
-        
-    }else if (data[i]=="+" || data[i]=="-" || data[i]=="x" || data[i]=="/"){
-        var operator=data[i];
-        leftOff=i;
-      haveNum1=true
+    updateDisplay(result, answer=true);
 }
-else if (haveNum1 && Number(data[i]) in validData){
-    num2 =data.slice(leftOff+1,i+1)
-
-    
-}}
 
 
 
@@ -69,7 +51,7 @@ console.log(num1)
             break;
         case "/":
             if (num2=="0"){
-                result="That is not possible."
+                var result="Infinity";
             }else{
             var result= divide(num1, num2);
         }
@@ -78,19 +60,21 @@ console.log(num1)
     document.querySelector(".display").textContent=result;
 }
 
-function updateDisplay(e){
-    
-      
-    
-    document.getElementById("display").textContent+=e.target.textContent;
+function updateDisplay(e, answer=false){
+    if(answer){
+document.getElementById("display").textContent=e;
+return;
 }
-var select=document.querySelectorAll(".numKey");
+document.getElementById("display").textContent+=e.target.textContent;
+	
+}
+var numKeys=document.querySelectorAll(".numKey");
 
-select.forEach(select =>
-    select.addEventListener('click', e => updateDisplay(e)));
-select=document.querySelectorAll(".opKey");
-select.forEach(select => 
-    select.addEventListener("click", function(e){document.querySelector(".display").textContent+=e.target.textContent; operation=e.target.textContent;}))
-select=document.querySelector(".equal");
-select.addEventListener("click",function(){operate(document.querySelector(".display").textContent)})
+numKeys.forEach(numKeys =>
+    numKeys.addEventListener('click', e => updateDisplay(e)));
+numKeys=document.querySelectorAll(".opKey");
+numKeys.forEach(numKeys => 
+    numKeys.addEventListener("click", function(e){document.querySelector(".display").textContent+=e.target.textContent; operation=e.target.textContent;}))
+numKeys=document.querySelector(".equal");
+numKeys.addEventListener("click",function(){solve(document.querySelector(".display").textContent)})
 console.log(document.querySelectorAll(".opKey")[0].textContent)
