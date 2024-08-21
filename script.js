11319ae59127b31fb428768ff629c40f74237309
Logan <92747680+celestial-bean@users.noreply.github.com>
2024-08-21 17:15:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,96 +1,126 @@
+function main() {
+  var lastKeyPressed = "";
 
+  function updateDisplay(e) {
+    document.getElementById("display").textContent += e.target.textContent;
+  }
 
-var num1=0, num2=0;
-function sub(num1, num2){
-    num1=Number(num1)
-    num2=Number(num2)
-return num1 - num2;
-}
-function add(num1, num2){
-    num1=Number(num1)
-    num2=Number(num2)
-return num1+num2;
-}
-function divide(num1, num2){
-    num1=Number(num1)
-    num2=Number(num2)
-return num1 / num2;
-}
-function mult(num1, num2){
-    num1=Number(num1)
-    num2=Number(num2)
-return num1 * num2;
-}
+  function setupButtons() {
+    var numkey = document.querySelectorAll(".numKey");
 
+    numkey.forEach(numkey =>
+      numkey.addEventListener('click', e => {
+        lastKeyPressed = e.target.textContent;
+        updateDisplay(e)
+      }));
+    var opkey = document.querySelectorAll(".opKey");
+    opkey.forEach(opkey =>
+      opkey.addEventListener("click", function(e) {
+        if (!(["+", "-", "/", "x"].includes(lastKeyPressed))) {
+          lastKeyPressed = e.target.textContent;
 
+          document.querySelector(".display").textContent += e.target.textContent;
 
-
-function operate(data){
-    var haveNum1=false;
-    data=data.replace(/\s/g,'')
-    
-    var validData=[1,2,3,4,5,6,7,8,9,0]
-    var num1=0;
-    var leftOff=0;
-    var num2=0;
-
-
-    for (var i=0;i<data.length; i++){
-        
-        if (!haveNum1 && Number(data[i]) in validData){
-        num1=data.slice(0,i+1)
-
-        
-    }else if (data[i]=="+" || data[i]=="-" || data[i]=="x" || data[i]=="/"){
-        var operator=data[i];
-        leftOff=i;
-      haveNum1=true
-}
-else if (haveNum1 && Number(data[i]) in validData){
-    num2 =data.slice(leftOff+1,i+1)
-
-    
-}}
-
-
-
-console.log(num1)
-    console.log(operator)
-    console.log(num2)
-    switch(operator){
-        case "+":
-            var result= add(num1, num2);
-            break;
-        case "-":
-            var result= sub(num1, num2);
-            break;
-        case "x":
-            var result= mult(num1, num2);
-            break;
-        case "/":
-            if (num2=="0"){
-                result="That is not possible."
-            }else{
-            var result= divide(num1, num2);
         }
-            break;
+      }))
+    var equalkey = document.querySelector(".equal");
+    equalkey.addEventListener("click", function() {
+      operate(document.querySelector(".display").textContent)
+    })
+  }
+
+  function sub(num) {
+    var result = num.split("-");
+    result = Number(num[0]) - Number(num[1]);
+    return result;
+
+  }
+
+  function add(num) {
+    var result = num.split("+");
+    result = Number(result[0]) + Number(result[1]);
+    return result;
+
+  }
+
+  function divide(num) {
+    var result = num.split("/");
+    if (Number(num[1]) == 0) {
+      return 0;
     }
-    document.querySelector(".display").textContent=result;
+    result = Number(num[0]) / Number(num[1]);
+    return result;
+  }
+
+  function mult(num) {
+    var result = num.split("x");
+    result = Number(result[0]) * Number(result[1]);
+    return result;
+  }
+
+  function operate(data) {
+    let result = 0;
+    let terms = 0;
+    var validData = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+    data = data.replace(/\s/g, '');
+
+    console.log(`data: ${data}`)
+    //12x34
+    let opIndex = -1;
+    let operation = "";
+
+    opIndex = Math.min([data.indexOf("x"), data.indexOf("/")].filter((num) => {
+      return num != -1
+    }))
+    if (opIndex == -1) {
+      opIndex = Math.min([opIndex, data.indexOf("+"), data.indexOf("-")].filter((num) => {
+        return num != -1
+      }));
+    }
+
+  
+
+
+  //get first
+  //12x34
+  var firstIndex;
+  var lastIndex;
+  console.log(`op index: ${opIndex}`);
+  for (var firstIndex = opIndex - 1; validData.includes(data[firstIndex]) && firstIndex >=
+    0; firstIndex--) {}
+firstIndex++
+  console.log(`first index: ${firstIndex}`);
+  for (var lastIndex = opIndex+1; lastIndex < data.length && validData.includes(data[lastIndex]); lastIndex++) {}
+  // lastIndex--;
+  console.log(`last index: ${lastIndex}`);
+  if (lastIndex == data.length) {
+    var segment = data.slice(firstIndex);
+  } else {
+    var segment = data.slice(firstIndex, lastIndex + 1)
+  }
+  //
+
+  console.log(`calculating: ${segment}`)
+  switch (data[opIndex]) {
+    case "x":
+      data = data.replace(segment, mult(segment));
+      break;
+    case "/":
+      data = data.replace(segment, divide(segment));
+      break;
+    case "+":
+      data = data.replace(segment, add(segment));
+      break;
+    case "-":
+      data = data.replace(segment, sub(segment));
+      break;
+    default:
+      document.querySelector(".display").textContent = data;
+      return;
+  }
+  operate(data);
 }
 
-function updateDisplay(e){
-    
-      
-    
-    document.getElementById("display").textContent+=e.target.textContent;
+setupButtons();
 }
-var select=document.querySelectorAll(".numKey");
-
-select.forEach(select =>
-    select.addEventListener('click', e => updateDisplay(e)));
-select=document.querySelectorAll(".opKey");
-select.forEach(select => 
-    select.addEventListener("click", function(e){document.querySelector(".display").textContent+=e.target.textContent; operation=e.target.textContent;}))
-select=document.querySelector(".equal");
-select.addEventListener("click",function(){operate(document.querySelector(".display").textContent)})
-console.log(document.querySelectorAll(".opKey")[0].textContent)
+main();
